e517f05e29f53535c274a8f54e64b6a226660937
SucrerSaler <75135592+SucrerSaler@users.noreply.github.com>
2021-02-09 17:38:07 +0100

    Ajout de la methode __contains__

--- tp2.py
+++ tp2.py
@@ -3,7 +3,9 @@ class Box:
     def __init__(self):
         self._contents = list()
 
+    def __contains__(self, machin):
+        return machin in self._contents
+
     def add(self, truc):
         self._contents.append(truc)
 
-    
