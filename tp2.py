9999a54b41a6c3ccc5724d9b296936c8d87e7844
SucrerSaler <75135592+SucrerSaler@users.noreply.github.com>
2021-02-09 17:30:41 +0100

    Ajout de test_box_in

--- tp2.py
+++ tp2.py
@@ -1,7 +1,9 @@
 class Box:
     
     def __init__(self):
-        self.contents = list()
+        self._contents = list()
 
     def add(self, truc):
-        self.contents.append(truc)
+        self._contents.append(truc)
+
+    
