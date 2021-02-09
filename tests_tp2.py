fb7a28b8cef35f89f8e1bb5e6afe1e07b6c1b181
SucrerSaler <75135592+SucrerSaler@users.noreply.github.com>
2021-02-09 17:53:35 +0100

    Ajout du test is_open

--- tests_tp2.py
+++ tests_tp2.py
@@ -21,3 +21,12 @@ def test_box_remove():
     b.add("truc2")
     b.remove("truc2")
     assert "truc2" not in b
+
+def test_box_is_open():
+    b = Box()
+    
+    b.open()
+    assert is_open()
+
+    b.close()
+    assert not is_open()
